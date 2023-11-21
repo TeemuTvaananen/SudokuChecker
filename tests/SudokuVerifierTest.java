1847210d0d1f36a776b9e1ff4f07f3c81f34edbc
Teemu_Vaananen <teemvaan@gmail.com>
2023-11-21 14:06:29 +0200

    sudoku done

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -10,6 +10,13 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 String c = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 String i = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+String tooLong = "4173698256321589479587243168254371697915864323469127582896435715732916841648752932";
+String tooShort = "4173698256321589479587243168254371697915864323469127582896435715732916";
+String emojis = "❤💔💌💕💞💓💗💖💘💝💟💜💛💚💙❤💔💌💕💞💓💗💖💘💝💟💜💛💚💙❤💔💌💕💞💓💖💘💟💜💛💙";
+String breaksRule2 = "123456789156789123789123456214365897365897214897214365531642978642978531978531642";
+String breaksRule3 = "417369825632158947958724316825437169791586432346912758289643571573291683164875294";
+String breaksRule4 = "417369825632158947958724316825437169791586432346912758289643571573291684164875239";
+
 SudokuVerifier v = new SudokuVerifier();
 
 	@Test
@@ -22,8 +29,40 @@ SudokuVerifier v = new SudokuVerifier();
 	public void testIncorrectString() {
 		int a = v.verify(i);
 		assertEquals("incorrect string", a, -2);
+	}
 		
+	@Test
+	public void testTooLong() {
+		int result  = v.verify(tooLong);
+		assertEquals("incorrect string", -1, result);
 	}
 	
+	@Test 
+	public void testTooShort() {
+		int result = v.verify(tooShort);
+		assertEquals("Incorrect string", -1, result);
+	}
 	
+	@Test
+   public void testRule2() {
+		int result = v.verify(breaksRule2);
+		assertEquals("Incorrect string", -2, result);
+	}
+	@Test
+   public void testRule3() {
+		int result = v.verify(breaksRule3);
+		assertEquals("Incorrect string",-3, result);
+	}
+	
+	@Test 
+	public void testRule4() {
+		int result = v.verify(breaksRule4);
+		assertEquals("Incorrect string",-4, result);
+	}
+	
+	@Test
+	public void testEmojis() {
+		 int result = v.verify(emojis);
+	     assertEquals("String with non-numeric chars should return 1", 1, result);
+	}
 }
